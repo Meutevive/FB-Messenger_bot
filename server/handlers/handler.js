f5b4ee9286ff004a940610c2dbed985c1b55a3fb
Meutevive <christianmbappe033@gmail.com>
2023-08-16 13:37:40 +0200

    revamp the code to send querry to gpt3 api

--- server/handlers/handler.js
+++ server/handlers/handler.js
@@ -1,37 +1,18 @@
-//const dialogflow = require('@google-cloud/dialogflow'); //diaglof flow
-const OpenAIApi = require('openai'); //import open ai api
-const openai = new OpenAIApi(process.env.OPENAI_API_KEY);
-const sessionClient = new dialogflow.SessionsClient();
+const { OpenAIApi } = require('openai'); //import open ai api
+const openai = new OpenAIApi({ api_key: process.env.OPENAI_API_KEY });
 const callSendAPI = require('../services/messenger'); // import messenger function
-const { text } = require('body-parser');
 
 // Handle message events
 function handleMessage(sender_id, receivedMessage, isMessenger = false) {
     return new Promise((resolve, reject) => {
-        // Create the session path using the Google project ID and sender ID
-        const sessionPath = sessionClient.projectAgentSessionPath(process.env.GOOGLE_PROJECT_ID, sender_id);
-
-        // Create the request object for Dialogflow
-        const request = {
-            session: sessionPath,
-            queryInput: {
-                text: {
-                    text: receivedMessage.text,
-                    languageCode: 'en-US' // update this to the language you want to use
-                }
-            },
-        }
-
-        // Send request to Dialogflow
+        // Send request to OpenAI
         openai.complete({
             model: 'gpt-3.5-turbo',
             prompt: receivedMessage.text,
             max_tokens: 50
         }).then(response => {
             let responseText = response.choices[0].text.trim();
-
-            // Vous pouvez ajouter une logique supplémentaire ici si vous voulez, comme une vérification de la confiance
-
+            console.log('OpenAI response:', responseText);
             // Check if the message came from Messenger or custom UI
             if (isMessenger) {
                 // Send OpenAI response to user on Messenger
@@ -51,7 +32,6 @@ function handleMessage(sender_id, receivedMessage, isMessenger = false) {
             // Reject promise with the error
             reject(err);
         });
-
     });
 }
 
